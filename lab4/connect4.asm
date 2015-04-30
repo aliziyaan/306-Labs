799e66e406a8f9bf95b54fc1d689fd1ae6cb0cfc
Ali Ziyaan <alimomin@utexas.edu>
2015-04-29 21:50:06 -0500

    just updating lab4 code

--- lab4/connect4.asm
+++ lab4/connect4.asm
@@ -363,8 +363,95 @@ ROW6		.stringz	"------"			;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 DISPLAY_BOARD
 	
+	ST R7, SAVER7
+	ST R0, SAVER0
+	ST R1, SAVER1
+	ST R2, SAVER2
+
+	LEA R1, ROW1
+LP1	LDR R0, R1, #0
+	BRz N1
+	OUT
+	LD R0, ASCII_SPACE
+	OUT
+	ADD R1, R1, #1
+	BR LP1
+N1	LD R0, ENT
+	OUT
+	BR RW2
+
+RW2	LEA R1, ROW2
+LP2	LDR R0, R1, #0
+	BRz N2
+	OUT
+	LD R0, ASCII_SPACE
+	OUT
+	ADD R1, R1, #1
+	BR LP2
+N2	LD R0, ENT
+	OUT
+	BR RW3
+	
+RW3	LEA R1, ROW3
+LP3	LDR R0, R1, #0
+	BRz N3
+	OUT
+	LD R0, ASCII_SPACE
+	OUT
+	ADD R1, R1, #1
+	BR LP3
+N3	LD R0, ENT
+	OUT
+	BR RW4
+
+RW4	LEA R1, ROW4
+LP4	LDR R0, R1, #0
+	BRz N4
+	OUT
+	LD R0, ASCII_SPACE
+	OUT
+	ADD R1, R1, #1
+	BR LP4
+N4	LD R0, ENT
+	OUT
+	BR RW5
+
+RW5	LEA R1, ROW5
+LP5	LDR R0, R1, #0
+	BRz N5
+	OUT
+	LD R0, ASCII_SPACE
+	OUT
+	ADD R1, R1, #1
+	BR LP5
+N5	LD R0, ENT
+	OUT
+	BR RW6
+
+RW6	LEA R1, ROW6
+LP6	LDR R0, R1, #0
+	BRz N6
+	OUT
+	LD R0, ASCII_SPACE
+	OUT
+	ADD R1, R1, #1
+	BR LP6
+N6	LD R0, ENT
+	OUT
+	BR D1
+
+D1	LD R7, SAVER7
+	LD R0, SAVER0
+	LD R1, SAVER1
+	LD R2, SAVER2
+
 	RET
 
+SAVER7	.BLKW 1
+SAVER0	.BLKW 1
+SAVER2	.BLKW 1
+SAVER1	.BLKW 1
+ENT 	.FILL x000D
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	GAME_OVER						;
 ;	description:	checks WINNER and outputs the proper	;
@@ -386,7 +473,73 @@ GAME_OVER
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 CHECK_VALID
 
+	ST R1, SVE_R1
+	ST R2, SVE_R2
+	ST R3, SVE_R3
+
+	ADD R0, R0, #0
+	BRz INV
+	LD R1, NG30
+	ADD R1, R0, R1
+	BRz INV
+
+	ADD R2, R1, #-1
+	LEA R3, ROW1
+	ADD R2, R3, R2
+	LDR R2, R2, #0
+	LD R3, ASCII_NEGHYP
+	ADD R2, R3, R2
+	BRnp INV
+	
+	LD R2, NG1
+	ADD R3, R1, R2
+	BRz FND
+	
+	LD R2, NG2
+	ADD R3, R1, R2
+	BRz FND
+
+	LD R2, NG3
+	ADD R3, R1, R2
+	BRz FND
+
+	LD R2, NG4
+	ADD R3, R1, R2
+	BRz FND
+	
+	LD R2, NG5
+	ADD R3, R1, R2
+	BRz FND
+
+	LD R2, NG6
+	ADD R3, R1, R2
+	BRz FND
+
+	BR INV
+
+INV AND R6, R6, #0
+	BR DNE
+
+FND AND R6, R6, #0
+	ADD R6, R6, R1
+	BR DNE
+
+DNE LD R1, SVE_R1
+	LD R2, SVE_R2
+	LD R3, SVE_R3
+
 	RET
+
+SVE_R1	.BLKW 1
+SVE_R2	.BLKW 1
+SVE_R3	.BLKW 1
+NG30	.FILL x-30
+NG1		.FILL x-1
+NG2		.FILL x-2
+NG3 	.FILL x-3
+NG4		.FILL x-4
+NG5		.FILL x-5
+NG6		.FILL x-6
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;USE THE FOLLOWING TO ACCESS THE BOARD!!!;;;;;;;;;;;;;;;;;;
@@ -398,6 +551,88 @@ BOARD_PTR	.blkw	1
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;	GIMME_OFFSET_ASCII					;
+;	description:	calculates offset and fetches ascii value.	;
+;	inputs:		R6 has the column of the last move.	;
+;			R5 has the row of the last move.	;
+;	outputs:	R2 has ascii value from the input grid.			;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+GIMME_OFFSET_ASCII
+
+		ST R1, S_VER1
+		ST R3, S_VER3
+		ST R4, S_VER4
+
+		AND R2, R2, #0
+
+		LD R1, NG1
+		ADD R1, R5, R1
+		BRnp FML1
+		ADD R2, R2, #0
+		ADD R3, R6, #-1
+		ADD R2, R3, R2
+		BR D_NE
+
+FML1	LD R1, NG2
+		ADD R1, R5, R1
+		BRnp FML2
+		ADD R2, R2, #7
+		ADD R3, R6, #-1
+		ADD R2, R3, R2
+		BR D_NE
+
+FML2	LD R1, NG3
+		ADD R1, R5, R1
+		BRnp FML3
+		ADD R2, R2, #14
+		ADD R3, R6, #-1
+		ADD R2, R3, R2
+		BR D_NE
+
+FML3	LD R1, NG4
+		ADD R1, R5, R1
+		BRnp FML4
+		LD R4, NUM21
+		ADD R2, R2, R4
+		ADD R3, R6, #-1
+		ADD R2, R3, R2
+		BR D_NE
+
+FML4	LD R1, NG5
+		ADD R1, R5, R1
+		BRnp FML5
+		LD R4, NUM28
+		ADD R2, R2, R4
+		ADD R3, R6, #-1
+		ADD R2, R3, R2
+		BR D_NE						
+
+FML5	LD R1, NG6
+		ADD R1, R5, R1
+		LD R4, NUM35
+		ADD R2, R2, R4
+		ADD R3, R6, #-1
+		ADD R2, R3, R2
+		BR D_NE
+
+D_NE 	LD R3, BOARD_PTR
+		ADD R2, R3, R2
+		LDR R2, R2, #0
+
+		LD R1, S_VER1
+		LD R3, S_VER3
+		LD R4, S_VER4
+
+	RET
+
+S_VER1	.BLKW 1
+S_VER3 	.BLKW 1
+S_VER4	.BLKW 1
+NUM21	.FILL #21
+NUM28	.FILL #28
+NUM35	.FILL #35
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	CHECK_HORIZONTAL					;
 ;	description:	horizontal check.			;
 ;	inputs:		R6 has the column of the last move.	;
@@ -407,8 +642,76 @@ BOARD_PTR	.blkw	1
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 CHECK_HORIZONTAL
 
-	RET
+		ST R7, SAV_R7
+		ST R0, SAV_R0
+		ST R1, SAV_R1
+		ST R2, SAV_R2
+		ST R3, SAV_R3
+		ST R5, SAV_R5
+		ST R6, SAV_R6
 
+		JSR GIMME_OFFSET_ASCII
+		AND R0, R0, #0
+		AND R3, R3, #0
+		ADD R3, R3, R2
+		NOT R3, R3
+		ADD R3, R3, #1
+		ADD R0, R0, #1
+		AND R1, R1, #0
+		ADD R1, R1, R6
+		AND R4, R4, #0
+
+GAB		ADD R6, R6, #1
+		AND R7, R7, #0
+		ADD R7, R7, #-7
+		ADD R7, R6, R7
+		BRzp LEFT
+		JSR GIMME_OFFSET_ASCII
+		ADD R2, R3, R2
+		BRnp LEFT
+		ADD R0, R0, #1
+		AND R7, R7, #0
+		ADD R7, R7, #-4
+		ADD R7, R0, R7
+		BRz WM
+		BR GAB
+
+LEFT	ADD R1, R1, #-1
+		BRz NM
+		ADD R6, R1, #0
+		JSR GIMME_OFFSET_ASCII
+		ADD R2, R3, R2
+		BRnp NM
+		ADD R0, R0, #1
+		AND R7, R7, #0
+		ADD R7, R7, #-4
+		ADD R7, R0, R7
+		BRz WM
+		BR LEFT
+
+WM		ADD R4, R4, #1
+		BR WOO
+
+NM		AND R4, R4, #0
+		BR WOO
+
+WOO		LD R7, SAV_R7
+		LD R0, SAV_R0
+		LD R1, SAV_R1
+		LD R2, SAV_R2
+		LD R3, SAV_R3
+		LD R5, SAV_R5
+		LD R6, SAV_R6			
+
+		RET
+
+SAV_R7	.BLKW 1
+SAV_R0	.BLKW 1
+SAV_R1	.BLKW 1
+SAV_R2	.BLKW 1
+SAV_R3	.BLKW 1		
+SAV_R5	.BLKW 1
+SAV_R6	.BLKW 1
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	CHECK_VERTICAL						;
 ;	description:	vertical check.				;
@@ -418,6 +721,54 @@ CHECK_HORIZONTAL
 ;				1, otherwise.			;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 CHECK_VERTICAL
+
+		ST R7, SAV_R7
+		ST R0, SAV_R0
+		ST R1, SAV_R1
+		ST R2, SAV_R2
+		ST R3, SAV_R3
+		ST R5, SAV_R5
+		ST R6, SAV_R6
+
+		JSR GIMME_OFFSET_ASCII
+		ADD R3, R2, #0
+		NOT R3, R3
+		ADD R3, R3, #1
+		AND R0, R0, #0
+		AND R7, R7, #0
+		AND R1, R1, #0
+		ADD R0, R0, #1
+		ADD R1, R1, R7
+
+GRB		ADD R5, R5, #1
+		AND R7, R7, #0
+		ADD R7, R7, #-7
+		ADD R7, R5, R7
+		BRzp NFM
+		JSR GIMME_OFFSET_ASCII
+		ADD R2, R3, R2
+		BRnp NFM
+		ADD R0, R0, #1
+		AND R7, R7, #0
+		ADD R7, R7, #-4
+		ADD R7, R0, R7
+		BRz WFM
+		BR GRB
+
+NFM		AND R4, R4, #0
+		BR HOO
+
+WFM		AND R4, R4, #0
+		ADD R4, R4, #1
+		BR HOO
+
+HOO		LD R7, SAV_R7
+		LD R0, SAV_R0
+		LD R1, SAV_R1
+		LD R2, SAV_R2
+		LD R3, SAV_R3
+		LD R5, SAV_R5
+		LD R6, SAV_R6	
 	
 	RET
 
